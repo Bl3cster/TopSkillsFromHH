38935977b9c0fd3133016f6a37d25864238439bc
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-11-11 15:46:58 +0300

    environment

---
+++ src/main/java/ru/chameleon/topskillsfromhhru/TopSkillsFromHHruApplication.java
@@ -0,0 +1,13 @@
+package ru.chameleon.topskillsfromhhru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopSkillsFromHHruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopSkillsFromHHruApplication.class, args);
+    }
+
+}
