28a5556276a9702fc27bce5d395fc81cd2f00160
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-11-13 21:54:27 +0300

    first functional, need to be reviewed

---
+++ src/main/java/ru/chameleon/topskillsfromhhru/Skills.java
@@ -0,0 +1,47 @@
+package ru.chameleon.topskillsfromhhru;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import ru.yaal.project.hhapi.HhApi;
+import ru.yaal.project.hhapi.dictionary.Constants;
+import ru.yaal.project.hhapi.search.SearchException;
+import ru.yaal.project.hhapi.search.parameter.Text;
+import ru.yaal.project.hhapi.vacancy.Vacancy;
+import ru.yaal.project.hhapi.vacancy.VacancyList;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Skills {
+
+    public static void main(String[] args) throws SearchException, IOException {
+        System.out.println(getVacancies("java разработчик"));
+    }
+
+    public static Map<String, Integer> getVacancies(String request) throws SearchException, IOException {
+        // get vacancies by name (limit = 100)
+        VacancyList vacancies = HhApi.search(100, new Text(request, Constants.VacancySearchFields.VACANCY_NAME));
+        Map<String, Integer> mapOfSkills = new HashMap<>();
+        // iterate by vacancies
+        for (Vacancy vacancy : vacancies) {
+            ObjectNode objectNode = new ObjectMapper().readValue(vacancy.getUrl(), ObjectNode.class);
+            // iterate by skills
+            for (JsonNode skill : objectNode.findValues("key_skills")) {
+                // iterate by skill names
+                for (String name : skill.findValuesAsText("name")) {
+                    mapOfSkills.put(name, mapOfSkills.getOrDefault(name, 0) + 1);
+                }
+            }
+        }
+        // return sorted map of skills
+        return mapOfSkills.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+}
--- src/main/java/ru/chameleon/topskillsfromhhru/TopSkillsFromHHruApplication.java
+++ src/main/java/ru/chameleon/topskillsfromhhru/TopSkillsFromHHruApplication.java
@@ -9,5 +9,4 @@ public class TopSkillsFromHHruApplication {
     public static void main(String[] args) {
         SpringApplication.run(TopSkillsFromHHruApplication.class, args);
     }
-
 }
