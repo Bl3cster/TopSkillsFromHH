de2aa759f63c23edc59a9724f67c214e1d2aba46
Игнатьев Максим Сергеевич <mignatev@phoenixit.ru>
2022-11-14 03:02:08 +0300

    new method

--- src/main/java/ru/chameleon/topskillsfromhhru/Skills.java
+++ src/main/java/ru/chameleon/topskillsfromhhru/Skills.java
@@ -3,7 +3,6 @@ package ru.chameleon.topskillsfromhhru;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import ru.yaal.project.hhapi.HhApi;
 import ru.yaal.project.hhapi.dictionary.Constants;
 import ru.yaal.project.hhapi.search.ISearch;
 import ru.yaal.project.hhapi.search.ISearchParameter;
@@ -14,7 +13,6 @@ import ru.yaal.project.hhapi.search.parameter.Text;
 import ru.yaal.project.hhapi.vacancy.Salary;
 import ru.yaal.project.hhapi.vacancy.Vacancy;
 import ru.yaal.project.hhapi.vacancy.VacancyList;
-import ru.yaal.project.hhapi.vacancy.VacancyPage;
 import ru.yaal.project.hhapi.vacancy.VacancySearch;
 
 import java.io.IOException;
@@ -34,59 +32,68 @@ public class Skills {
     static String currency;
     static String experience;
 
-    public static void main(String[] args) throws SearchException, IOException, InterruptedException {
-        Scanner stringScanner = new Scanner(System.in);
+    public static void main(String[] args) throws SearchException, IOException {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите название профессии для поиска ключевых навыков. \n Для выхода из программы введите \"end\" \n");
-        String request = stringScanner.nextLine();
+        String request = scanner.nextLine();
         if (request.equals("end")) {
             return;
         }
         System.out.println("""
-                Введите цифру 0, если хотите получать зарплату в рублях;\s
-                Введите 1, если хотите получать зарплату в долларах;\s
-                Введите 2, если хотите получать зарплату в евро;\s
-                Введите 3, если хотите получать зарплату в манатах\s
-                Введите 4, если хотите получать зарплату в белорусских рублях\s
-                Введите 5, если хотите получать зарплату в тенге\s
-                Введите 6, если хотите получать зарплату в гривнах
+                Введите цифру 0, если хотите узнать подборку навыков по вакансии без учёта зарплаты и прочих параметров;\s
+                Введите 1, если желаете задать параметры поиска.
                 """);
-        int currencyFind = stringScanner.nextInt();
-        switch (currencyFind) {
-            case 0 -> currency = "RUR";
-            case 1 -> currency = "USD";
-            case 2 -> currency = "EUR";
-            case 4 -> currency = "AZN";
-            case 5 -> currency = "BYR";
-            case 6 -> currency = "KZT";
-            case 7 -> currency = "UAH";
+        int dreamJob = scanner.nextInt();
+        if (dreamJob == 0) {
+            System.out.println(getVacancies(request));
+        } else {
+            System.out.println("""
+                    Введите цифру 0, если хотите получать зарплату в рублях;\s
+                    Введите 1, если хотите получать зарплату в долларах;\s
+                    Введите 2, если хотите получать зарплату в евро;\s
+                    Введите 3, если хотите получать зарплату в манатах\s
+                    Введите 4, если хотите получать зарплату в белорусских рублях\s
+                    Введите 5, если хотите получать зарплату в тенге\s
+                    Введите 6, если хотите получать зарплату в гривнах
+                    """);
+            int currencyFind = scanner.nextInt();
+            switch (currencyFind) {
+                case 0 -> currency = "RUR";
+                case 1 -> currency = "USD";
+                case 2 -> currency = "EUR";
+                case 4 -> currency = "AZN";
+                case 5 -> currency = "BYR";
+                case 6 -> currency = "KZT";
+                case 7 -> currency = "UAH";
 
+            }
+            System.out.println("Введите минимальную желаемую зарплату: \n");
+            int minSalary = scanner.nextInt();
+            System.out.println("""
+                    Введите цифру 0, если у Вас нет опыта;\s
+                    Введите 1, если Ваш опыт от 1 года до 3 лет;\s
+                    Введите 2, если Ваш опыт от 3 до 6 лет;\s
+                    Введите 3, если Ваш опыт более 6 лет.
+                    """);
+            int experienceFind = scanner.nextInt();
+            switch (experienceFind) {
+                case 0 -> experience = "noExperience";
+                case 1 -> experience = "between1And3";
+                case 2 -> experience = "between3And6";
+                case 3 -> experience = "moreThan6";
+            }
+            System.out.println(getVacancies(request, minSalary, experience, currency));
         }
-        System.out.println("Введите минимальную желаемую зарплату: \n");
-        int minSalary = stringScanner.nextInt();
-        System.out.println("""
-                Введите цифру 0, если у Вас нет опыта;\s
-                Введите 1, если Ваш опыт от 1 года до 3 лет;\s
-                Введите 2, если Ваш опыт от 3 до 6 лет;\s
-                Введите 3, если Ваш опыт более 6 лет.
-                """);
-        int experienceFind = stringScanner.nextInt();
-        switch (experienceFind) {
-            case 0 -> experience = "noExperience";
-            case 1 -> experience = "between1And3";
-            case 2 -> experience = "between3And6";
-            case 3 -> experience = "moreThan6";
-        }
-        System.out.println(getVacancies(request, minSalary, experience, currency));
         main(args);
     }
-    public static Map<String, Integer> getVacancies(String request, int minSalary, String experiences) throws SearchException, IOException, InterruptedException {
+    public static Map<String, Integer> getVacancies(String request, int minSalary, String experiences, String currency) throws SearchException, IOException {
         // get vacancies by name (limit = 100)
         ISearchParameter text = new Text(request, Constants.VacancySearchFields.VACANCY_NAME);
-        ISearchParameter salary = new Salary(minSalary, null, Constants.Currency.RUR);
+        ISearchParameter salary = new Salary(minSalary, null, CURRENCIES.getById(currency));
         ISearchParameter experience = EXPERIENCES.getById(experiences);
         ISearchParameter onlyWithSalary = Constants.OnlyWithSalary.ON;
         //ISearchParameter schedule = Constants.Schedule.REMOTE;
-        Map<String, Integer> mapOfSkills = new HashMap<>();
+        Map<String, Integer> mapOfSkills;
 
         List<Vacancy> vacancies = new ArrayList<>();
         for(int i = 0; i < 20; i++) {
@@ -95,10 +102,40 @@ public class Skills {
                     .addParameter(salary)
                     .addParameter(experience)
                     .addParameter(onlyWithSalary)
-                    //.addParameter(schedule)
                     .addParameter(new SearchParameterBox(SearchParamNames.PAGE, String.valueOf(i)));
             vacancies.addAll(search.search());
         }
+        mapOfSkills = countSkills(vacancies);
+        // return sorted map of skills
+        return mapOfSkills.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+
+    public static Map<String, Integer> getVacancies(String request) throws SearchException, IOException {
+        // get vacancies by name (limit = 100)
+        ISearchParameter text = new Text(request, Constants.VacancySearchFields.VACANCY_NAME);
+        Map<String, Integer> mapOfSkills;
+        List<Vacancy> vacancies = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            ISearch<VacancyList> search = new VacancySearch(100)
+                    .addParameter(text)
+                    .addParameter(new SearchParameterBox(SearchParamNames.PAGE, String.valueOf(i)));
+            vacancies.addAll(search.search());
+        }
+        mapOfSkills = countSkills(vacancies);
+        // return sorted map of skills
+        return mapOfSkills.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+
+    private static Map<String, Integer> countSkills (List<Vacancy> vacancies) throws IOException {
+        Map<String, Integer> mapOfSkills = new HashMap<>();
         // iterate by vacancies
         for (Vacancy vacancy : vacancies) {
             ObjectNode objectNode = new ObjectMapper().readValue(vacancy.getUrl(), ObjectNode.class);
@@ -110,11 +147,6 @@ public class Skills {
                 }
             }
         }
-        // return sorted map of skills
-        return mapOfSkills.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .limit(10)
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        return mapOfSkills;
     }
 }
